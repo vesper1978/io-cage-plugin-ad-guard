34e37429d2575a55d466e987f5f486fc9684c991
root <root@storage.projexion.net>
2021-05-17 08:50:58 -0300

    Slight issues

--- post_install.sh
+++ post_install.sh
@@ -6,9 +6,7 @@ pw useradd -n adguard -m -c "Ad Guard Home user" -s /usr/sbin/nologin -w no
 mkdir -p /home/adguard/.config/adguard
 chown -R adguard:adguard /home/adguard/
 
-cd /home/adguard/.config/adguard && fetch https://static.adguard.com/adguardhome/release/AdGuardHome_freebsd_amd64.tar.gz
-
-tar zxvf AdGuardHome_freebsd_amd64.tar.gz
+cd /home/adguard/.config/adguard && fetch https://static.adguard.com/adguardhome/release/AdGuardHome_freebsd_amd64.tar.gz && gunzip AdGuardHome_freebsd_amd64.tar.gz && tar xf AdGuardHome_freebsd_amd64.tar && rm AdGuardHome_freebsd_amd64.tar
 
 # Configure the services
 sysrc -f /etc/rc.conf adguardhome_enable="YES"
