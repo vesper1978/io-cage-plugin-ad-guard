a55870a989d1d3411176373d2af8c8e0e2607e13
vesper1978 <28059535+vesper1978@users.noreply.github.com>
2021-05-17 05:51:36 -0300

    Create post_install.sh

---
+++ post_install.sh
@@ -0,0 +1,12 @@
+!/bin/sh
+
+# Prepare the system
+
+pw useradd -n adguard -m -c "Ad Guard Home user" -s /usr/sbin/nologin -w no
+mkdir -p /home/adguard/.config/adguard
+chown -R adguard:adguard /home/adguard/
+
+cd /home/adguard/.config/adguard && fetch https://static.adguard.com/adguardhome/release/AdGuardHome_freebsd_amd64.tar.gz
+
+tar zxvf AdGuardHome_freebsd_amd64.tar.gz
+
