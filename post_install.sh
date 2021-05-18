82ac48dc02b3b7b5782860d367e5ad841642b0f4
vesper1978 <28059535+vesper1978@users.noreply.github.com>
2021-05-18 08:30:42 -0300

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -9,7 +9,7 @@ chown -R adguard:adguard /home/adguard/
 cd /home/adguard/.config/adguard && fetch https://static.adguard.com/adguardhome/release/AdGuardHome_freebsd_amd64.tar.gz && gunzip AdGuardHome_freebsd_amd64.tar.gz && tar xf AdGuardHome_freebsd_amd64.tar && rm AdGuardHome_freebsd_amd64.tar
 # Configure the services
 sysrc -f /etc/rc.conf adguardhome_enable="YES"
-sysrc -f /etc/rc.conf adguardhome_user="adguard"
+
 
 # Create pid folder
 mkdir /var/run/adguard
